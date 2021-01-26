5da40e854a9b9ec6ce2d54e5fdd2d5bb581742c7
WWilliam <myskywan80@gmail.com>
2021-01-26 16:29:50 +0100

    finalisation du CV

--- assets/myjs/script.js
+++ assets/myjs/script.js
@@ -1,10 +1,12 @@
 //document.getElementById('CV').style.display = "none";
+document.getElementById('project').style.display = "none";
 
 function Check() {
     var stock_value = document.getElementById('text-submit').value;
 
     if (stock_value == 'unmdp') {
         document.getElementById('CV').style.display = 'block';
+        document.getElementById('project').style.display = "block";
         document.getElementById('protect').style.display = 'none';
     } else {
         document.getElementById('HiddenText').innerHTML = 'ERREUR veuillez r\u00e9essayer.';
