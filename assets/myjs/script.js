7e4c3ec882d3c5a2feb0180049d8891f660bc474
WWilliam <myskywan80@gmail.com>
2021-01-27 14:13:17 +0100

    fix responsive commencement de la partie projets

--- assets/myjs/script.js
+++ assets/myjs/script.js
@@ -1,4 +1,4 @@
-//document.getElementById('CV').style.display = "none";
+document.getElementById('CV').style.display = "none";
 document.getElementById('project').style.display = "none";
 
 function Check() {
@@ -6,9 +6,15 @@ function Check() {
 
     if (stock_value == 'unmdp') {
         document.getElementById('CV').style.display = 'block';
-        document.getElementById('project').style.display = "block";
+        document.getElementById('project').style.display = 'block';
         document.getElementById('protect').style.display = 'none';
     } else {
         document.getElementById('HiddenText').innerHTML = 'ERREUR veuillez r\u00e9essayer.';
+    }
+}
+
+function Alert() {
+    if (window.confirm('Attention le site que vous voulez acc\u00e9der peut \u00eatre lent \u00e0 charger il est possible qu\'il faudra la recharger une deuxi\u00e8me fois !')) {
+        document.location = 'https://williamwarnet80.000webhostapp.com/';
     }
 }
