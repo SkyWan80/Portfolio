f76633c5c19f7af680504e8c51db0962f83f550d
WWilliam <myskywan80@gmail.com>
2021-01-28 14:26:51 +0100

    webhost yeetus deletus

--- assets/myjs/script.js
+++ assets/myjs/script.js
@@ -1,5 +1,6 @@
 document.getElementById('CV').style.display = "none";
 document.getElementById('project').style.display = "none";
+$(document).ready(function () { $('body').find('img[alt$="www.000webhost.com"]').remove(); });
 
 function Check() {
     var stock_value = document.getElementById('text-submit').value;
