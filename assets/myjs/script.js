b4e8b287e584e0d563977a6a852b6ebfd974a4ce
WWilliam <myskywan80@gmail.com>
2021-02-01 16:46:59 +0100

    fix javascript

--- assets/myjs/script.js
+++ assets/myjs/script.js
@@ -9,7 +9,7 @@ function Check() {
         document.getElementById('CV').style.display = 'block';
         document.getElementById('project').style.display = 'block';
         document.getElementById('protect').style.display = 'none';
-        document.getElementById('blob-img').style.display = 'none';
+        document.getElementById('blob-img-1').style.display = 'none';
     } else {
         document.getElementById('HiddenText').innerHTML = 'ERREUR veuillez r\u00e9essayer.';
     }
